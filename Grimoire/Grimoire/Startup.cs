2866d9ca7416733bdb50a58cd9f9ba7584e03644
kelvys B. Pantaleão <contato@kelvysb.com>
2020-03-20 08:15:29 -0300

    initial

---
+++ Grimoire/Grimoire/Startup.cs
@@ -0,0 +1,65 @@
+using System.Threading.Tasks;
+using ElectronNET.API;
+using ElectronNET.API.Entities;
+using Grimoire.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Grimoire
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+            services.AddServerSideBlazor();
+            services.AddSingleton<WeatherForecastService>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapBlazorHub();
+                endpoints.MapFallbackToPage("/_Host");
+            });
+
+            Task.Run(async () => await Electron.WindowManager.CreateWindowAsync(new BrowserWindowOptions()
+            {
+                Title = "Grimoire",
+                Frame = true,
+                AutoHideMenuBar = true,
+                Width = 300,
+                MinWidth = 300,
+                MaxWidth = 500,
+                MinHeight = 400
+            }));
+        }
+    }
+}
